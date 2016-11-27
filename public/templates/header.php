0abd841a2f6c8d36999feb31c914c72bffa7312c
Horacio Espinosa <dar5_0094@hotmail.com>
2016-11-26 22:22:30 -0500

    header urls2

--- public/templates/header.php
+++ public/templates/header.php
@@ -20,7 +20,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="nav">
-                    <a href="index.html" class="logo">
+                    <a href="/" class="logo">
                         <img src="img/theme-1/logo_dark.png" alt="lets travel">
                     </a>
                     <div class="nav-menu-icon">
